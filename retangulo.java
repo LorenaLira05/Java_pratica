683be03908cd2e958725086cfd81fce5587d99c6
LorenaLira05 <naheelee2005@gmail.com>
2025-09-10 13:57:48 -0300

    Create retangulo.java

---
+++ retangulo.java
@@ -0,0 +1,13 @@
+package geometria;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	Retangulo retangulo1 = new Retangulo(3,2);
+	
+	System.out.println("Area do retangulo: " + retangulo1.area());
+	System.out.println("Perimetro: " + retangulo1.perimetro());
+	
+	}
+}
