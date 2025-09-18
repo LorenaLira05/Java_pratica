2d96e01de8121a205a77bce6969b7c7a42c6862e
LorenaLira05 <naheelee2005@gmail.com>
2025-09-18 15:21:15 -0300

    Update CarroBasico.java

--- CarroBasico.java
+++ CarroBasico.java
@@ -1,1 +1,4 @@
-
+/** Classe de domínio: CarroBasico. O que deve representar? 
+Carro que liga/desliga, acelera/freira e mostra velocidade atual. 
+Demonstre: dois carros com sequências de uso distintas.
+**/
