fa69c94646f6af8cb3998ce0a0b054c1ef56d2f3
LorenaLira05 <naheelee2005@gmail.com>
2025-09-20 18:43:02 -0300

    Update Liquidificador.java

--- Liquidificador.java
+++ Liquidificador.java
@@ -1,61 +1,47 @@
 package ads.coding.lista.ex20;
 
 public class Liquidificador {
-	boolean ligado;
-	  int velocidade; // 1 = baixo, 2  =médio, 3 = alto
+    boolean ligado;
+    int velocidade; // 1 a 3
 
-	  //construtor
-	  //O liquidificador começa desligado e na velocidade 0
-	  public Liquidificador() {
-	    ligado = false;
-	    velocidade = 0;
-	  } 
+    // Construtor
+    public Liquidificador() {
+        ligado = false;
+        velocidade = 0;
+    }
 
-	  //Método para ligar o liquidificador
-	    public void ligar(){
-	      if(ligado) {
-	        System.out.println("O liquidificador já está ligado");
-	      } else {
-	        ligado = true;
-	        System.out.println("Ligando... liquidificador ligado");
-	      }
-	    }
+    // Método para Ligar
+    public void ligar() {
+        if (!ligado) {
+            ligado = true;
+            velocidade = 1;
+            System.out.println("Ligando... Liquidificador ligado na velocidade " + velocidade);
+        } else {
+            System.out.println("O liquidificador já está ligado");
+        }
+    }
 
-	  //Método para desligar o liquidificador
-	   public void desligar(){
-	      if(!ligado) {
-	        System.out.println("O liquidificador já está desligado");
-	      } else {
-	        ligado = false;
-	        velocidade = 0;
-	        System.out.println("Desligando... liquidificador desligado");
-	      }
-	    }
+    // Método para Desligar
+    public void desligar() {
+        if (ligado) {
+            ligado = false;
+            velocidade = 0;
+            System.out.println("Desligando... Liquidificador desligado.");
+        } else {
+            System.out.println("O liquidificador já está desligado");
+        }
+    }
 
-	  //Método para aumentar a velocidade do ventilador
-	     public void aumentarVelocidade(){
-		    if(!ligado) {
-		      System.out.println("Não é possivel aumentar a velocidade quando o liquidificador está desligado");
-		    } else if (velocidade <3) {
-		    	velocidade ++ ;
-		    	System.out.println("Aumentando a velocidade... Velocidade: " + velocidade );
-		    } else { System.out.println("Velocidade já está no máximo");
-	      }
-		  }
-
-	   //Método para diminuir a velocidade do ventilador
-	    public void diminuirVelocidade(){
-		    if(!ligado) {
-		      System.out.println("Não é possivel diminuir a velocidade quando o liquidificador está desligado");
-		    } else if (velocidade > 1) {
-		    	velocidade -- ;
-		    	System.out.println("Diminuindo a velocidade... Velocidade: " + velocidade );
-		    } else { System.out.println("Velocidade já está no mínimo");
-	      }
-		  }
-	    
-	    //status
-	    public void status(){
-	    System.out.println("liquidificador " + (ligado ? "ligado" : "desligado") + ", velocidade: " + velocidade);
-	}
+    // Método para bater
+    public void bater() {
+        if (ligado) {
+            if (velocidade >= 1 && velocidade <= 3) {
+                System.out.println("Batendo! O liquidificador está funcionando na velocidade " + velocidade + ".");
+            } else {
+                System.out.println("Velocidade inválida! Só é permitido de 1 a 3.");
+            }
+        } else {
+            System.out.println("Tentando bater... mas o liquidificador está desligado!");
+        }
+    }
 }
