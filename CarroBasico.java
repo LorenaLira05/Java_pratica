8548acb502b83c24d8b9de8590f75c1c169441a7
LorenaLira05 <naheelee2005@gmail.com>
2025-09-20 10:51:50 -0300

    Update CarroBasico.java

--- CarroBasico.java
+++ CarroBasico.java
@@ -1,7 +1,3 @@
-/** Classe de domínio: CarroBasico. O que deve representar? 
-Carro que liga/desliga, acelera/freira e mostra velocidade atual. 
-Demonstre: dois carros com sequências de uso distintas.
-**/
 package ads.coding.lista.ex03;
 
 public class CarroBasico {
@@ -27,7 +23,7 @@ public class CarroBasico {
 	  
 	//Método para desligar o carro
 	  public void desligar(){
-	    if (ligado == false) {
+	    if (!ligado) {
 	      System.out.println("O carro já está desligado");
 	    } else { 
 	    	ligado = false;
@@ -39,7 +35,7 @@ public class CarroBasico {
 	  
 	//Método para acelerar o carro
 	  public void acelerar(){
-	    if(ligado == false) {
+	    if(!ligado) {
 	      System.out.println("Não é possivel acelerar um carro desligado");
 	    } else {
 	    	velocidade +=10;
@@ -63,27 +59,3 @@ public class CarroBasico {
 	  }
 	  
 }
-
- public static void main(String[] args) {
-   //Criando o primeiro carro
-   CarroBasico c1 = new CarroBasico();
-     c1.ligar();
-     c1.acelerar();
-     c1.acelerar();
-     c1.freiar();
-     c1.freiar();
-     c1.desligar();
-     c1.status();
-   
-   //Criando segundo carro
-     CarroBasico c2 = new Carrobasico();
-     c2.status();
-     c2.acelerar();
-     c2.ligar();
-     c2.acelerar();
-     c2.freiar();
-     c2.desligar();
-     c2.status();
-     
-     
- }
