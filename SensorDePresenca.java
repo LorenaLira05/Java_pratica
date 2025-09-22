0bafc55ede4806694c9631ca7792c11e88f906b7
LorenaLira05 <naheelee2005@gmail.com>
2025-09-22 20:51:54 -0300

    Update SensorDePresenca.java

--- SensorDePresenca.java
+++ SensorDePresenca.java
@@ -1,1 +1,26 @@
+package ads.coding.lista.ex28;
+
+public class SensorDePresenca {
+    private boolean ativo;
+
+    public SensorDePresenca() {
+        ativo = false;
+    }
+
+    public void ativar() { ativo = true; }
+    public void desativar() { ativo = false; }
+
+    public void detectarPresenca() {
+        if (ativo) System.out.println("Presença detectada!");
+        else System.out.println("Sensor inativo");
+    }
+
+    public static void main(String[] args) {
+        SensorDePresenca sensor = new SensorDePresenca();
+
+        sensor.detectarPresenca(); // inativo
+        sensor.ativar();
+        sensor.detectarPresenca(); // ativo
+    }
+}
 
